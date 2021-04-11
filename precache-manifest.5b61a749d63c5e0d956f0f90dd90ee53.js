d0795d79c39defc6571490568b8eca8ee5feafaf
Chiraag Anand <67359421+Snail664@users.noreply.github.com>
2021-04-11 11:14:45 +0800

    Updates

---
+++ precache-manifest.5b61a749d63c5e0d956f0f90dd90ee53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca19a08f14bb37dbb8ec87a083fd3643",
+    "url": "/teslaClone/index.html"
+  },
+  {
+    "revision": "e48fc4cf08aa398a4a16",
+    "url": "/teslaClone/static/css/main.6fe022f9.chunk.css"
+  },
+  {
+    "revision": "b56700dc78ede8837039",
+    "url": "/teslaClone/static/js/2.f224a8aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/teslaClone/static/js/2.f224a8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48fc4cf08aa398a4a16",
+    "url": "/teslaClone/static/js/main.98716799.chunk.js"
+  },
+  {
+    "revision": "f21f8a1e84cb4c1870a2",
+    "url": "/teslaClone/static/js/runtime-main.4e982cda.js"
+  },
+  {
+    "revision": "e19c20e966bde501f94e41cd0322dbe8",
+    "url": "/teslaClone/static/media/Gotham Medium.e19c20e9.otf"
+  },
+  {
+    "revision": "7261d83c93d6ae9cb305badf6f6ed43b",
+    "url": "/teslaClone/static/media/hiclipart.com.7261d83c.png"
+  }
+]);
